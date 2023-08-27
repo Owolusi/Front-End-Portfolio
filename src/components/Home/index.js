a072d8e47081205b549b02354d3e58a1010cf101
Owolusi Lucky <109273986+Owolusi@users.noreply.github.com>
2023-08-26 22:02:23 -0700

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+
+const Home=()=>{
+    
+    return(
+        <div className="container home-page">
+                    <div className="text-zone">
+
+                        <h1>hi, I'm </h1>
+                        <img src={LogoTitle} alt="developer" />
+                        Mighty-Tech
+
+                        <br />
+                        Front End Web Developer
+                        </div>
+
+
+        </div>
+    )
+    }
+export default  Home
