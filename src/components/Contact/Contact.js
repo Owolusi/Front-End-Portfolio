0604e32f62e84d388f42e1fbdcd22f897ce32cc4
Owolusi Lucky <109273986+Owolusi@users.noreply.github.com>
2023-09-01 00:38:45 +0100

    added maps details

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -96,20 +96,20 @@ const Contact = () => {
           </div>
         </div>
         <div className="info-map">
-          Slobodan Gajić,
+          Ondo-State,
           <br />
-          Serbia,
+          Nigeria,
           <br />
-          Branka RadiČevića 19, 22000 <br />
-          Sremska Mitrovica <br />
+          Royal Birds Street, 0s <br />
+          Alagbaka Akure <br />
           <br />
           <span>m.bunmi5@gmail.com</span>
         </div>
         <div className="map-wrap">
           <MapContainer center={[44.96366, 19.61045]} zoom={13}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            <Marker position={[7.265173, 5.201644]}>
+              <Popup>Mighty lives here, come over for a cup of coffee :)</Popup>
             </Marker>
           </MapContainer>
         </div>
