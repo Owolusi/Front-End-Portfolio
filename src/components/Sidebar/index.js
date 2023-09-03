ebde1ffa6b6e4bd3ae7c173823b6b6fba2e84357
Owolusi Lucky <109273986+Owolusi@users.noreply.github.com>
2023-09-03 05:29:14 +0100

    mobile responsive3 homepage

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,8 +4,9 @@ import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faHome, faUser, faSuitcase,faClose, faBars,} from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faTwitter, faYoutube,faDiscord, faSkype,faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope, faHome, faUser, faSuitcase,faClose, faBars, faBookReader, faFile,} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faTwitter, faYoutube,faDiscord, faSkype,faLinkedin, faBuromobelexperte} from '@fortawesome/free-brands-svg-icons'
+import Resume from '../../assets/images/Lucky-Resume.jpg'
 
 const Sidebar = () => {
   const [showNav, setShowNav] = useState(false);
@@ -50,6 +51,29 @@ const Sidebar = () => {
         >
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
+
+
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+
+          to={Resume} download='Resume.jpg'
+
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faFile} color="#4d4d4e" />
+        </NavLink>
+
+        
+        <NavLink
+          activeclassname="active"
+          className="publication-link"
+          to="https://linktr.ee/mighty_scribe" target='_blank'
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faBookReader} color="#4d4d4e" />
+        </NavLink>
+
         <FontAwesomeIcon 
           onClick={() => setShowNav(false)}
           icon={faClose}
